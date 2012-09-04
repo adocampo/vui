2f19ae1373e9f10fdd2764f335ba7f269b29b128
J.A. Nache <nache.nache@gmail.com>
2012-09-04 21:41:26 +0200

    Pinceladas de codigo en minutos libres

--- makeModel/vuilib/mmutils/__init__.py
+++ makeModel/vuilib/mmutils/__init__.py
@@ -2,16 +2,27 @@
 # Writed by J.A. NachE <nache.nache@gmail.com>
 # Under one license, ask me for more information.
 
-import fileinput
+import fileinput, os
 
 class mmutils:
+
+
+	def __init__(self):
+		self.HDMan_path = NULL
+		self.HLEd_path = NULL
+		self.HCopy_path = NULL
+		self.HCompV_path = NULL
+		self.HERest_path = NULL
+		self.HHEd_path = NULL
+		self.HVite_path = NULL
+
 
 	def getPrompts(self, file):
 		for line in fileinput.input([file]):
 			lines.append([ line.split()[0] , ' '.join(line.split()[1:]) ])
 		return lines
 
-	def locateHTK (self):
+	def locateHTK (self, paths=[]):
 		pass
 
 	def setHDMan_path(self, path):
@@ -28,4 +39,27 @@ class mmutils:
 		self.HHEd_path = path
 	def setHVite_path(self, path):
 		self.HVite_path = path
+
+	def getHDMan_path(self, path):
+		return self.HDMan_path
+	def getHLEd_path(self, path):
+		return self.HLEd_path
+	def getHCopy_path(self, path):
+		return self.HCopy_path
+	def getHCompV_path(self, path):
+		return self.HCompV_path
+	def getHERest_path(self, path):
+		return self.HERest_path
+	def getHHEd_path(self, path):
+		return self.HHEd_path
+	def getHVite_path(self, path):
+		return self.HVite_path
+
+
+
+
+
+
+
+
 

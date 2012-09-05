61d4b3679d4548ffb291206075c5643c7ef1c7b6
J.A. Nache <nache.nache@gmail.com>
2012-09-05 16:06:02 +0200

    adding some qt code to gui, dialog to select files

--- makeModel/makeModelGUI.py
+++ makeModel/makeModelGUI.py
@@ -4,7 +4,7 @@
 
 import sys, os
 sys.path.append(os.path.abspath(os.path.dirname(sys.argv[0])))
-import vuilib.mmutils 
+from vuilib import mmutils
 from PyQt4 import Qt
 
 class vuiGUI(Qt.QMainWindow):
@@ -13,20 +13,63 @@ class vuiGUI(Qt.QMainWindow):
 		super(vuiGUI, self).__init__(None)	
 		self.setWindowTitle("make Model GUI")
 
+		layoutFiles = {}
+		layoutFiles['HDMan'] = Qt.QHBoxLayout()
+		layoutFiles['HLEd'] = Qt.QHBoxLayout()
+		layoutFiles['HCopy'] = Qt.QHBoxLayout()
+		layoutFiles['HCompV'] = Qt.QHBoxLayout()
+		layoutFiles['HERest'] = Qt.QHBoxLayout()
+		layoutFiles['HHEd'] = Qt.QHBoxLayout()
+		layoutFiles['HVite'] = Qt.QHBoxLayout()
+
+		self.labelFiles['HDMan'] = Qt.QLabel()
+		self.labelFiles['HLEd'] = Qt.QLabel()
+		self.labelFiles['HCopy'] = Qt.QLabel()
+		self.labelFiles['HCompV'] = Qt.QLabel()
+		self.labelFiles['HERest'] = Qt.QLabel()
+		self.labelFiles['HHEd'] = Qt.QLabel()
+		self.labelFiles['HVite'] = Qt.QLabel()
+		
+		buttonFiles['HDMan'] = Qt.QPushButton("Buscar...")
+		buttonFiles['HLEd'] = Qt.QPushButton("Buscar...")
+		buttonFiles['HCopy'] = Qt.QPushButton("Buscar...")
+		buttonFiles['HCompV'] = Qt.QPushButton("Buscar...")
+		buttonFiles['HERest'] = Qt.QPushButton("Buscar...")
+		buttonFiles['HHEd'] = Qt.QPushButton("Buscar...")
+		buttonFiles['HVite'] = Qt.QPushButton("Buscar...")
+
+		self.connect(buttonFiles['HDMan'], Qt.SIGNAL("clicked()"), self.FileDialog, 'HDMan')
+		self.connect(buttonFiles['HLEd'], Qt.SIGNAL("clicked()"), self.FileDialog, 'HLEd')
+		self.connect(buttonFiles['HCopy'], Qt.SIGNAL("clicked()"), self.FileDialog, 'HCopy')
+		self.connect(buttonFiles['HCompV'], Qt.SIGNAL("clicked()"), self.FileDialog, 'HCompV')
+		self.connect(buttonFiles['HERest'], Qt.SIGNAL("clicked()"), self.FileDialog, 'HERest')
+		self.connect(buttonFiles['HHEd'], Qt.SIGNAL("clicked()"), self.FileDialog, 'HHEd')
+		self.connect(buttonFiles['HVite'], Qt.SIGNAL("clicked()"), self.FileDialog, 'HVite')
+
+
+		for ['HDMan', 'HLEd', 'HCopy', 'HCompV', 'HERest', 'HHEd', 'HVite'] in binName:
+			layoutFiles[binName].addWidget(Qt.QLabel(binName))
+			layoutFiles[binName].addWidget(self.labelFiles[binName])
+			layoutFiles[binName].addWidget(buttonFiles[binName])
+
+
 		layoutV = Qt.QVBoxLayout()
-
 		self.textEdit = Qt.QTextEdit()
-
 		layoutV.addWidget(self.textEdit)
-		self.button =  Qt.QPushButton("Test")
-		self.connect(self.button, Qt.SIGNAL("clicked()"), self.test)
-		layoutV.addWidget(self.button)
+
+
+		for ['HDMan', 'HLEd', 'HCopy', 'HCompV', 'HERest', 'HHEd', 'HVite'] in binName:
+			layoutV.addWidget(layoutFiles[binName])
 
 		widget=Qt.QWidget()
 		widget.setLayout(layoutV)
 
 		self.setCentralWidget(widget)
 
+
+	def fileDialog(self,binName):
+		#Bug? if none is returnetd?
+		self.labelFiles[binName] = QtGui.QFileDialog.getOpenFileName(self, 'Open file', '/home')
 
 	def test(self):
 		print ("Killing in the name of...")

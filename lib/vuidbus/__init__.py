e546ca0d660edcc394fce0a676fd20d7dcd2c6fc
J.A. Nache <nache.nache@gmail.com>
2013-02-14 12:44:49 +0100

    lib for manage dbus system

---
+++ lib/vuidbus/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# Writed by J.A. NachE <nache.nache@gmail.com>
+# Under one license, ask me for more information.
+
+
+
+class vuidbus:
+
+	def __init_(self):
+		#
+		print ("vuidbus started")

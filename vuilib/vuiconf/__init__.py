1dd252e6cd76c7b75aebb423fc5a8cd8ffe88e0c
J.A. Nache <nache.nache@gmail.com>
2013-02-14 13:21:34 +0100

    start of vuiconf class

--- vuilib/vuiconf/__init__.py
+++ vuilib/vuiconf/__init__.py
@@ -5,3 +5,18 @@
 class vuiconf:
 	def __init__(self):
 		print("vuiconf started")
+
+
+def getConfFile():
+"""return file location
+this function will detect if
+it is windows or linux/unix system"""
+
+class vuiConfFile:
+	#def __init__(fileLocation=getConfFile()): #probe later if this work
+	def __init__(fileLocation=None):
+		if fileLocation == None:
+			#open getConfFile()
+		else:
+			#open fileLocation
+	

2a741bede65953ce9186e5ed7e24d46e1ba173c1
J.A. Nache <nache.nache@gmail.com>
2013-02-14 13:54:04 +0100

    start of vuidbus lib

--- vuilib/vuidbus/__init__.py
+++ vuilib/vuidbus/__init__.py
@@ -2,10 +2,18 @@
 # Writed by J.A. NachE <nache.nache@gmail.com>
 # Under one license, ask me for more information.
 
-
+import dbus
 
 class vuidbus:
-
-	def __init_(self):
+	def __init__(self):
 		#
 		print ("vuidbus started")
+
+
+class absdbus:
+	#WARNING:
+	#look for new dbus API in 0.81.0
+	#
+	def __init__(self):
+		self.Desktop_dbus = dbus.SessionBus()
+		self.System_dbus = dbus.SystemBus()

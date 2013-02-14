04559306b3695b7c04d096c4980add24a15d3a46
J.A. Nache <nache.nache@gmail.com>
2013-02-14 12:46:05 +0100

    added vuiconf lib

---
+++ lib/vuiconf/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# Writed by J.A. NachE <nache.nache@gmail.com>
+# Under one license, ask me for more information.
+
+class vuiconf:
+	def __init__(self):
+		print("vuiconf started")
